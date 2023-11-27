c674a00bc6dfe3cb69735029be3baf50b9f6cdf1
ivannew <ivanenriquesotorodriguez@gmail.com>
2023-11-26 21:21:26 -0700

    Agregar archivos de proyecto.

---
+++ Controllers/AuthorsController.cs
@@ -0,0 +1,36 @@
+﻿using libreria_IESR.Data.Models.Services;
+using libreria_IESR.Data.Services;
+using libreria_IESR.Data.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace libreria_IESR.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorsController : ControllerBase
+    {
+        private AuthorsService _authorsService;
+
+        public AuthorsController(AuthorsService authorsService)
+        {
+            _authorsService = authorsService;
+        }
+
+        [HttpPost("add-author")]
+        public IActionResult AddAuthor([FromBody] AuthorVM author)
+        {
+            _authorsService.AddAuthor(author);
+            return Ok(); 
+        }
+
+        [HttpGet("get-author-with-books-by-id")]
+        public IActionResult GetAuthorWithBooks(int id)
+        {
+            var response = _authorsService.GetAuthorWithBooks(id);
+            return Ok(response);
+        }
+
+
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,75 @@
+using libreria_IESR.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Microsoft.EntityFrameworkCore;
+using libreria_IESR.Data.Models;
+using libreria_IESR.Data.Models.Services;
+using libreria_IESR.Data.Services;
+
+namespace libreria_IESR
+{
+    public class Startup
+    {
+        public string ConnectionString { get; set; }
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            ConnectionString = configuration.GetConnectionString("DefaultConnectionString");
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+            services.AddControllers();
+            //Configurar DbContext con SQL
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(ConnectionString));
+            //Configurar para ser usado 
+            services.AddTransient<BooksService>();
+            services.AddTransient<AuthorsService>();
+            services.AddTransient<PublisherService>();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "libreria_IESR", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "libreria_IESR v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+            //AppDbInitialer.Seed(app); 
+        }
+    }
+}
